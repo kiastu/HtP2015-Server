e57dac8abd32a9ae971f0291695ee7e9fa05d1bb
David Kong (LCL) <David.Kong@loblaw.ca>
2015-08-16 04:29:44 -0400

    removed s3, damn amazon didnt want to work for us. Photos are uploading and saving on the server now. Able to upload larger pphotos due to the the enl the larger POST body size

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(morgan('dev'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb'}));
 app.use(methodOverride());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(compression());
--- routes/index.js
+++ routes/index.js
@@ -2,64 +2,28 @@
  * GET home page.
  */
 var Twitter = require('twitter'),
-	request = require('request'),
-	fs = require('fs'),
-	s3 = require('s3');
-	
+    request = require('request'),
+    fs = require('fs');
+
 var hastag = '#hack-the-planet'
 
-var s3client = s3.createClient({
-  maxAsyncS3: 20,     // this is the default 
-  s3RetryCount: 3,    // this is the default 
-  s3RetryDelay: 1000, // this is the default 
-  multipartUploadThreshold: 20971520, // this is the default (20 MB) 
-  multipartUploadSize: 15728640, // this is the default (15 MB) 
-  s3Options: {
-    accessKeyId: "AKIAIP5CDOJX6DS7PKGA",
-    secretAccessKey: "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",
-    // any other options are passed to new AWS.S3() 
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property 
-  },
-});
-
-var params = {
-  localFile: "some/local/file",
- 
-  s3Params: {
-    Bucket: "s3 bucket name",
-    Key: "some/remote/file",
-    // other options supported by getObject 
-    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property 
-  },
-};
-var downloader = s3client.downloadFile(params);
-downloader.on('error', function(err) {
-  console.error("unable to download:", err.stack);
-});
-downloader.on('progress', function() {
-  console.log("progress", downloader.progressAmount, downloader.progressTotal);
-});
-downloader.on('end', function() {
-  console.log("done downloading");
-});
-	
 var client = new Twitter({
-	consumer_key: 'Rj6bc5IqvPGKIct1DunbNzG0p',
-	consumer_secret: 'dCfSdJOG2tW0CnnhHGZFEGCpOTa0jIveykbPAFFRwTh112aBkD',
-	access_token_key: '3315791730-DPkIbVGigEzV64ObAhtxX63zAAzhn8ApSyPOWdo',
-	access_token_secret: 'sN7tFi7hYqNHi9oKgAtnBmsg63olosIsMRjf7TEkH1wYb'
-	});
+    consumer_key: 'Rj6bc5IqvPGKIct1DunbNzG0p',
+    consumer_secret: 'dCfSdJOG2tW0CnnhHGZFEGCpOTa0jIveykbPAFFRwTh112aBkD',
+    access_token_key: '3315791730-DPkIbVGigEzV64ObAhtxX63zAAzhn8ApSyPOWdo',
+    access_token_secret: 'sN7tFi7hYqNHi9oKgAtnBmsg63olosIsMRjf7TEkH1wYb'
+});
 
-exports.index = function(req, res) {
+exports.index = function (req, res) {
     res.render('index');
 };
 
-exports.partials = function(req, res) {
+exports.partials = function (req, res) {
     var name = req.params.name;
     res.render('partials/' + name);
 };
 
-exports.push = function(req, res) {
+exports.push = function (req, res) {
     var options = {
         url: 'https://api.parse.com/1/push',
         headers: {
@@ -67,29 +31,30 @@ exports.push = function(req, res) {
             'X-Parse-Rest-API-Key': 'agXbECxwVrDgE2yqYLqpHN2Axf7Wn25vrFsr68mr',
             'Content-Type': 'application/json'
         },
-		json: true,
-		body: {
-				where:{},
-				data: {alert:'Yiu'}}
+        json: true,
+        body: {
+            where: {},
+            data: {
+                alert: 'Yiu'
+            }
+        }
     };
 
-    request.post(options, function(error, response, body) {
-		console.log(body)
+    request.post(options, function (error, response, body) {
+        console.log(body)
         if (!error && response.statusCode == 200) {
-			console.log("Complete")
+            console.log("Complete")
             console.log(body) // Show the HTML for the Google homepage.
-			res.send("success")
+            res.send("success")
+        } else {
+            console.log(error)
         }
-		else
-		{
-		console.log(error)
-		}
     })
 
     function callback(error, response, body) {
         if (!error && response.statusCode == 200) {
             var info = JSON.parse(body);
-            console.log(info.stargazers_count + " Stars");	
+            console.log(info.stargazers_count + " Stars");
             console.log(info.forks_count + " Forks");
         }
     }
@@ -101,37 +66,59 @@ exports.push = function(req, res) {
 
 // Objects for 
 
-exports.imgupdate = function(req, res) {
-	var jTweet = req.body;
-	console.log(jTweet)
-	fs.writeFileSync(jTweet.name,new Buffer(jTweet.img.toString(),"base64"));
-	var data = fs.readFileSync(jTweet.name);
-	
-	// Make post request on media endpoint. Pass file data as media parameter
-	client.post('media/upload', {media: data}, function(error, media, response){
+exports.imgupdate = function (req, res) {
 
-  if (!error) {
+    var body = req.body;
+    //download the image file.
 
-    // If successful, a media object will be returned.
-    console.log(media);
+    var twitterRequest = function () {
+        var data = fs.readFileSync("../snapshots/" + body.name);
+        // Make post request on media endpoint. Pass file data as media parameter
+        client.post('media/upload', {
+            media: data
+        }, function (error, media, response) {
 
-    // Lets tweet it
-    var status = {
-      status: jTweet.message,
-      media_ids: media.media_id_string // Pass the media id string
+            if (!error) {
+
+                // If successful, a media object will be returned.
+                console.log(media);
+
+                // Lets tweet it
+                var status = {
+                    status: jTweet.message,
+                    media_ids: media.media_id_string // Pass the media id string
+                }
+
+                client.post('statuses/update', status, function (error, tweet, response) {
+                    if (!error) {
+                        console.log(tweet);
+                    }
+                });
+
+                res.json({
+                    message: "Success"
+                });
+            } else {
+                console.log(error);
+            }
+
+        });
+
     }
+    console.log("Body:" + body.name);
+    fs.writeFile("snapshots/" + body.name, body.img, 'base64', function (err) {
+        if (!err) {
+            console.log("Great success! File was saved!");
+            twitterRequest()
 
-    client.post('statuses/update', status, function(error, tweet, response){
-      if (!error) {
-        console.log(tweet);
-      }
+        } else {
+            console.log(err);
+            console.log("Error, could not save file, something fucked up.");
+            res.status(500).send("Error");
+        }
     });
 
-  }
-  res.json({message:"Success"});
-});
 
-	
 }
 
 //jTweet
@@ -141,10 +128,12 @@ exports.imgupdate = function(req, res) {
 //	lat 
 //
 
-exports.tweet = function(req, res) {
-	client.post('statuses/update', {status: 'Hello, World!'}, function(error, tweet, response){
-	if (!error) {
-		console.log(tweet);
-	}
-	});
+exports.tweet = function (req, res) {
+    client.post('statuses/update', {
+        status: 'Hello, World!'
+    }, function (error, tweet, response) {
+        if (!error) {
+            console.log(tweet);
+        }
+    });
 }
